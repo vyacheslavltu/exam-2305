f138d8f6a8e9bc5430681b695d03079a52bb7527
Vyacheslav <106969028+vyacheslavltu@users.noreply.github.com>
2023-01-05 14:56:32 +0200

    edit errors

---
+++ client-ui/src/Contexts/Cities.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const Cities = createContext();
+export default Cities;
---
+++ client-ui/src/Contexts/Events.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const Events = createContext();
+export default Events;
